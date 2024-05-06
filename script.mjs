a149392e5fcebfa160da3f876b818c34d2923a04
JinZ <bbcy777@gmail.com>
2024-05-06 15:56:47 -0700

    updated try

--- script.mjs
+++ script.mjs
@@ -14,11 +14,8 @@ let plant_area = num_plants * min_space;
 console.log(`Space needed for all plants will be ${plant_area} square meters`);
 
 try {
-    if (plant_area < area) {
-        console.log(`All plants can fit in the amount of space available`);
-    }
-    else {
-        throw `Plants can't fit in the available space.`
+    if (plant_area > area) {
+        throw `Plants can't fit in the available space.`;
     }
 } catch (error) {
     console.log(error);
